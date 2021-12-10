e867b98014f669cc3033190829de3bc26c15d4d8
relkaddouri <rachid.elkaddouri1@gmail.com>
2021-12-10 11:38:05 +0100

    Search Form, Footer, Results Page : Adjusting Responsive Design

--- js/app.js
+++ js/app.js
@@ -4,3 +4,12 @@ $( "#close-btn" ).click(function() {
 $( "#open-btn" ).click(function() {
     $("#menu-mobile").css({"display":"flex"});
 });
+
+// FEATURES MODAL
+
+$( "#close-btn-features" ).click(function() {
+    $("#features-mobile").css({"display":"none"});
+});
+$( "#open-btn-features" ).click(function() {
+    $("#features-mobile").css({"display":"flex"});
+});
